17180fe70942339c5a04208b87008f7a6012f0e5
grey <starbux630@naver.com>
2023-08-09 21:34:45 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var (
+	baseURL  string = "https://www.jobkorea.co.kr/Search/?stext="
+	goLang   string = "golang"
+	javaLang string = "java&tabType=recruit"
+)
+
+func main() {
+	totalPages := getPageNumber()
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+	fmt.Println(totalPages)
+}
+
+func getPage(page int) {
+	pageURL := baseURL + javaLang + "&Page_No=" + strconv.Itoa(page+1)
+	fmt.Println("Requesting page: ", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find("li.list-post")
+
+	searchCards.Each(func(i int, s *goquery.Selection) {
+		s.Attr(".data-gno")
+	})
+
+}
+
+func getPageNumber() int {
+	pages := 0
+	res, err := http.Get(baseURL + javaLang)
+	checkErr(err)
+	checkCode(res)
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	doc.Find(".recruit-info .tplPagination.newVer.wide").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("ul li").Length()
+	})
+
+	defer res.Body.Close()
+
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		//kill the program
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with status code: ", res.StatusCode)
+	}
+}
