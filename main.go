ce9af0319a2fb4aadf019d857c0c63ead02d0332
grey <starbux630@naver.com>
2023-08-11 19:41:33 +0900

    fix: get all data on pages

--- main.go
+++ main.go
@@ -16,27 +16,33 @@ var (
 	javaLang string = "java&tabType=recruit"
 )
 
-type JsonData struct {
-	ID          string `json:"dimension42"`
-	Title       string `json:"dimension45"`
-	Location    string `json:"dimension46"`
-	Company     string `json:"dimension48"`
-	Sort        string `json:"dimension43"`
-	Work        string `json:"dimension44"`
-	Dimension65 string `json:"dimension65"`
-	Dimension66 string `json:"dimension66"`
-	Dimension70 string `json:"dimension70"`
-	Dimension47 string `json:"dimension47"`
+type extractJobData struct {
+	ID       string `json:"dimension42"`
+	Title    string `json:"dimension45"`
+	Location string `json:"dimension46"`
+	Company  string `json:"dimension48"`
+	Sort     string `json:"dimension43"`
+	Work     string `json:"dimension44"`
+	/*
+		Dimension65 string `json:"dimension65"`
+		Dimension66 string `json:"dimension66"`
+		Dimension70 string `json:"dimension70"`
+		Dimension47 string `json:"dimension47"`
+	*/
 }
 
 func main() {
+	var jobs []extractJobData
 	totalPages := getPageNumber()
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+	fmt.Printf("jobs: %+v\n", jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractJobData {
+	var jobs []extractJobData
 	pageURL := baseURL + javaLang + "&Page_No=" + strconv.Itoa(page+1)
 	fmt.Println("Requesting page: ", pageURL)
 	res, err := http.Get(pageURL)
@@ -48,29 +54,36 @@ func getPage(page int) {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	searchCards := doc.Find(".lists")
+	searchCards := doc.Find(".lists .clear .list-post")
 	fmt.Println("GET CARD")
-	searchCards.Each(func(i int, s *goquery.Selection) {
-		extractJob(s)
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job, _ := extractJob(card)
+		if job.ID == "" {
+			return
+		}
+		jobs = append(jobs, job)
+		fmt.Printf("jobs: %+v\n", jobs)
 	})
+	fmt.Println("GET CARD OVER")
+	return jobs
 }
 
-func extractJob(s *goquery.Selection) {
-	card := s.Find(".list-post")
+func extractJob(card *goquery.Selection) (extractJobData, error) {
 	id, _ := card.Attr("data-gno")
 	if id == "" {
-		return
+		return extractJobData{}, fmt.Errorf("id is empty")
 	}
 	infoData, _ := card.Attr("data-gainfo")
 	if infoData == "" {
-		return
+		return extractJobData{}, fmt.Errorf("infoData is empty")
 	}
-	var jsonData JsonData
+	var jsonData extractJobData
 	err := json.Unmarshal([]byte(infoData), &jsonData)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	fmt.Printf("jsonData: %+v\n", jsonData)
+	return jsonData, nil
 }
 
 func getPageNumber() int {
